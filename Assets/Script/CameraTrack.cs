b4b8e0ddf668fd0e820f071ecfea0e02aa561c6e
Addie Liu <liuaddie@gmail.com>
2021-09-23 23:31:06 +0800

    Two Players Control, Add Materials

--- Assets/Script/CameraTrack.cs
+++ Assets/Script/CameraTrack.cs
@@ -9,13 +9,15 @@ public class CameraTrack : MonoBehaviour
 
     //Quaternion rot;   //[To Be Check] Error: unity Object reference not set to an instance of an object
     Vector3 pos;
-    float speedPos = 3f;
-    float speedRot = 10f;
-
+    int camDistantZ = 6;
+    int camDistantY = 1;
+    float speedPos = 6f;
+    float speedRot = 40f;
+ 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -23,7 +25,7 @@ public class CameraTrack : MonoBehaviour
     {
         //----- Task 2 -----
         pos = Vector3.Lerp(transform.position,
-            new Vector3(target.position.x, target.position.y, target.position.z - 10),
+            new Vector3(target.position.x, target.position.y + camDistantY, target.position.z - camDistantZ),
             Time.deltaTime * speedPos);
         transform.position = pos;
 
--- Assets/Script/Control.cs
+++ Assets/Script/Control.cs
@@ -4,36 +4,43 @@ using UnityEngine;
 
 public class Control : MonoBehaviour
 {
+    public enum Players { Player1, Player2 };
+    public Players playerId;
+
     Vector3 playerPos;
+
+
     // Start is called before the first frame update
     void Start()
     {
         playerPos = transform.position;
-        
+        //print(playerId);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        //print(playerPos.x);
+        if (Input.GetKey(KeyCode.A) & string.Equals(playerId, Players.Player1) || Input.GetKey(KeyCode.Semicolon) & string.Equals(playerId, Players.Player2))
         {
-            playerPos.x -= 0.4f;
+            playerPos.x -= (playerPos.x > -50) ? 0.4f : 0f;
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.S) & string.Equals(playerId, Players.Player1) || Input.GetKey(KeyCode.Quote) & string.Equals(playerId, Players.Player2))
         {
-            playerPos.x += 0.4f;
+            playerPos.x += (playerPos.x < 50) ? 0.4f : 0f;
         }
 
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.Q) & string.Equals(playerId, Players.Player1) || Input.GetKey(KeyCode.LeftBracket) &string.Equals(playerId, Players.Player2))
         {
             transform.Rotate(0, 0, 1);
         }
 
-        if(Input.GetKey(KeyCode.E))
+        if(Input.GetKey(KeyCode.W) & string.Equals(playerId, Players.Player1) || Input.GetKey(KeyCode.RightBracket) & string.Equals(playerId, Players.Player2))
         {
             transform.Rotate(0, 0, -1);
         }
+
 
         transform.position = playerPos;
     }
