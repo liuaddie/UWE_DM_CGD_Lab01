b393c74d1d3a12fac3d5c48b8c5eeeab02a00398
Addie Liu <liuaddie@gmail.com>
2021-09-23 21:49:37 +0800

    Initial Push for Assets

---
+++ Assets/Script/CameraTrack.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrack : MonoBehaviour
+{
+
+    public Transform target;
+
+    //Quaternion rot;   //[To Be Check] Error: unity Object reference not set to an instance of an object
+    Vector3 pos;
+    float speedPos = 3f;
+    float speedRot = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //----- Task 2 -----
+        pos = Vector3.Lerp(transform.position,
+            new Vector3(target.position.x, target.position.y, target.position.z - 10),
+            Time.deltaTime * speedPos);
+        transform.position = pos;
+
+        Quaternion currentRot = transform.rotation;
+        Quaternion targetRot = target.transform.rotation;
+        transform.rotation = Quaternion.RotateTowards(currentRot, targetRot, Time.deltaTime * speedRot);
+
+        //// [To Be Check] Error: unity Object reference not set to an instance of an object
+        //rot = Quaternion.Lerp(Camera.main.transform.rotation,
+        //    target.transform.rotation,
+        //    Time.deltaTime * speed);
+        //Camera.main.transform.rotation = rot;
+
+        ////-----Task 1-----
+        //transform.position = new Vector3(
+        //    target.position.x,
+        //    target.position.y,
+        //    target.position.z - 10);
+        //Camera.main.transform.rotation = target.transform.rotation;
+
+        ////-----Task 0-----
+        //transform.LookAt(target);
+    }
+}
---
+++ Assets/Script/Control.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    Vector3 playerPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerPos.x -= 0.4f;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerPos.x += 0.4f;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, 0, 1);
+        }
+
+        if(Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, 0, -1);
+        }
+
+        transform.position = playerPos;
+    }
+}
